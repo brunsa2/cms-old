8d83266452343f7e8ae1c6ac5e882eee3913258e
brunsa2 <brunsa2@gmail.com>
2009-10-20 19:25:16 -0500

    Reworking some changes to the user.php class
    Took out return values for the login function, but still might have to remove the error throwing that could be unnecessary for the logout function.

--- system/user.php
+++ system/user.php
@@ -78,11 +78,9 @@ class User {
       $_SESSION['username'] = $this->username;
       $_SESSION['passwordHash'] = $this->passwordHash;
       $this->isLoggedIn = 1;
-      return 1;
     } else {
       $_SESSION['isLoggedIn'] = 0;
       $this->isLoggedIn = 0;
-      return 0;
     }
   }
   
